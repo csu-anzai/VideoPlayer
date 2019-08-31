236e8678d66f38ccb5fadc88c2a4908a822ee5d6
Ahmed Basha <a7madalibasha@gmail.com>
2019-09-01 01:50:19 +0900

    Add implementation for VideoListViewController
    Implemented all the necessary parts in VideoListViewContorller
    to show the data provided by the viewModel.
    
    Decided to go with a UICollectionView because of its
    flowLayout's flexibility. Better transition into
    iPad support, etc.

--- QuipperVideoPlayer/VideoListViewController.swift
+++ QuipperVideoPlayer/VideoListViewController.swift
@@ -22,11 +22,79 @@ class VideoListViewController: UIViewController {
 
     weak var delegate: VideoListViewControllerDelegate?
     
+    private lazy var viewModel: VideoListViewModel = {
+        let viewModel = VideoListViewModel()
+        viewModel.dataChanged = { [weak self] in
+            DispatchQueue.main.async {
+                self?.collectionView.reloadData()
+            }
+        }
+        return viewModel
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let flowLayout = TopAlignedCollectionViewFlowLayout()
+        // ensure self sizing cells
+        flowLayout.estimatedItemSize = CGSize(width: view.bounds.width, height: 100)
+        flowLayout.itemSize = UICollectionViewFlowLayout.automaticSize
+        flowLayout.minimumLineSpacing = 16.0
+        return UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        setUpCollectionView()
     }
-
+    
+    private func setUpCollectionView() {
+        // view setup
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .white
+        // register cells
+        collectionView.register(VideoCell.self, forCellWithReuseIdentifier: String(describing: VideoCell.self))
+        // constraints
+        constrain(view, collectionView) { (view, collectionView) in
+            collectionView.edges == view.edges
+        }
+        // dataSource & delegate
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
 }
 
+//
+// MARK: - UICollectionViewDataSource
+//
+extension VideoListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numberOfVideos()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: VideoCell.self), for: indexPath) as? VideoCell else {
+            return VideoCell()
+        }
+        let title = viewModel.videoTitle(at: indexPath)
+        let presenterName = viewModel.videoPresenterName(at: indexPath)
+        let url = viewModel.videoThumbnailURL(at: indexPath)
+        cell.configure(title: title, presenterName: presenterName, thumbnailImageURL: url)
+        
+        return cell
+    }
+}
+
+//
+// MARK: - UICollectionViewDelegateFlowLayout
+//
+extension VideoListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let videoInfo = viewModel.videoInfo(at: indexPath)
+        delegate?.didSelect(videoInfo, viewController: self)
+    }
+}
