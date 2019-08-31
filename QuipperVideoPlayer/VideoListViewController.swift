75dfd42c6eb41fbf9c5d666505352e92a896cd0a
Ahmed Basha <a7madalibasha@gmail.com>
2019-08-31 22:41:31 +0900

    #7 Add the main navigation coordinator class
    Also, the needed delegate protocol and setup.

--- QuipperVideoPlayer/VideoListViewController.swift
+++ QuipperVideoPlayer/VideoListViewController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+import Cartography
+
+protocol VideoListViewControllerDelegate: class {
+    /// This is called when the user selects a specific Video.
+    ///
+    /// - Parameters:
+    ///   - videoInfo: The selected video information object.
+    ///   - viewController: The view controller in which the selection happend in.
+    func didSelect(_ videoInfo: VideoInfo, viewController: VideoListViewController)
+}
 
 class VideoListViewController: UIViewController {
 
+    weak var delegate: VideoListViewControllerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
