d053d1ffcef972ed381135b8193bdaf4bad6a60b
Ahmed Basha <a7madalibasha@gmail.com>
2019-08-31 15:25:23 +0900

    Initial commit

---
+++ QuipperVideoPlayer/VideoListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListViewController.swift
+//  QuipperVideoPlayer
+//
+//  Created by Ahmed Ali on 2019/08/28.
+//  Copyright © 2019 Ahmed Ali. All rights reserved.
+//
+
+import UIKit
+
+class VideoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+}
+
